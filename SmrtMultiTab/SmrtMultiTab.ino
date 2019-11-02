f0a92fd223d45bad674242a051427e3475332f17
jjkamd <jjkamd@naver.com>
2019-11-02 11:23:27 +0900

    Update SmrtMultiTab.ino
    이름추가

--- SmrtMultiTab/SmrtMultiTab.ino
+++ SmrtMultiTab/SmrtMultiTab.ino
@@ -39,7 +39,7 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "blynk에서 발급한 토큰"; //이선우
+char auth[] = "blynk에서 발급한 토큰"; //이선우 //장정권
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
