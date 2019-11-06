1372be653c6cb8963c7be7de7a6977c061b736df
nillili <ds1lph@gmail.com>
2019-11-06 10:04:32 +0900

    Update SmrtMultiTab.ino
    온습도계(DHT11) 연동
    - 디바이스에서 읽어 서버로 전송

--- SmrtMultiTab/SmrtMultiTab.ino
+++ SmrtMultiTab/SmrtMultiTab.ino
@@ -38,19 +38,27 @@
 #include <BlynkSimpleEsp8266.h>
 SimpleTimer timer;
 
+// 온습도 센서 init
+#include "DHT.h"
+#define DHTPIN 14
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
 
 
 //char auth[] = "0wB6ZPg2_r53NPFNKs8L7xA4KpjgsjFu"; //이선우
-//char auth[] = "3CmeaeOhhrnMFTHUKQVw0iO5eRarQFcl"; //홍상진
+char auth[] = "3CmeaeOhhrnMFTHUKQVw0iO5eRarQFcl"; //홍상진
 //char auth[] = "MN6w88J069vMz0WSRZyY1co_BpPGHg7x"; //장정권
 
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "jjangsvc";
-char pass[] = "123456789a";
+//char ssid[] = "jjangsvc";
+//char pass[] = "123456789a";
+char ssid[] = "neozio";
+char pass[] = "0536259956";
 
 const int ledPin =  4;
 int ledState = LOW; 
@@ -65,13 +73,14 @@ BLYNK_WRITE(V1) //Button Widget is writing to pin V1
 
 void setup()
 {
-
+  dht.begin();
+  
   pinMode(ledPin, OUTPUT);
   // Debug console
   Serial.begin(9600);//시리얼 포트
 
   Blynk.begin(auth, ssid, pass);
-  timer.setInterval(1000L, sendSensor);
+  timer.setInterval(2000L, sendSensor);
 }
 
 void loop()
@@ -83,6 +92,31 @@ void loop()
 
 void sendSensor()
 {
+
+  // 온습도 읽기
+
+  float h = dht.readHumidity();           // 습도     
+  float t = dht.readTemperature();        // 온도
+
+  if (isnan(h) || isnan(t)) 
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+
+  
+  Blynk.virtualWrite(V2, t);
+  Blynk.virtualWrite(V3, h);
+
+  //////
+  
   if(pinData == 1)
   {
     Serial.println("on");
