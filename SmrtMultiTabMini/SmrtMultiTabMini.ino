2c1c6e25f05b958db5b29daac91d57c0650dfa8b
nillili <ds1lph@gmail.com>
2020-11-29 23:39:05 +0900

    미완성 - 서버접속 안됨

--- SmrtMultiTabMini/SmrtMultiTabMini.ino
+++ SmrtMultiTabMini/SmrtMultiTabMini.ino
@@ -1,40 +1,7 @@
-/*************************************************************
-  Download latest Blynk library here:
-    https://github.com/blynkkk/blynk-library/releases/latest
-
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: http://www.blynk.cc
-    Sketch generator:           http://examples.blynk.cc
-    Blynk community:            http://community.blynk.cc
-    Follow us:                  http://www.fb.com/blynkapp
-                                http://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
-
- *************************************************************
-  This example runs directly on ESP8266 chip.
-
-  Note: This requires ESP8266 support package:
-    https://github.com/esp8266/Arduino
-
-  Please be sure to select the right ESP8266 module
-  in the Tools -> Board menu!
-
-  Change WiFi ssid, pass, and Blynk auth token to run :)
-  Feel free to apply it to any other example. It's simple!
- *************************************************************/
-
-/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
 #include "userinfo.h"
-//#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 SimpleTimer timer;
 
@@ -46,10 +13,6 @@ DHT dht(DHTPIN, DHTTYPE);
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-
-//char auth[] = "3"; //홍상진
-//char ssid[] = "n";
-//char pass[] = "0";
 
 extern const char auth[];
 extern const char ssid[];
@@ -99,21 +62,27 @@ BLYNK_WRITE(V8) //Button Widget is writing to pin V1
 void setup()
 {
   // Debug console
-  Serial.begin(9600);//시리얼 포트
+  Serial.begin(115200);//시리얼 포트
   
   
   pinMode(ledPin, OUTPUT);
   pinMode(RlyPin, OUTPUT);
 //  pinMode(SwhPin, INPUT);
-  
+ Serial.println("-------->1"); 
   digitalWrite(RlyPin, !relay_on);  
-
-  Blynk.begin(auth, ssid, pass);
   
-
+Serial.println("-------->2"); 
   timer.setInterval(2000L, sendSensor);
   
+  
+Serial.println("-------->3"); 
+  Blynk.begin(auth, ssid, pass);
+//  WiFi.begin(ssid, pass);  // Non-blocking if no WiFi available
+//  Blynk.config(auth);
+//  Blynk.connect();  
+Serial.println("-------->4");   
   dht.begin();  
+  
 }
 
 BLYNK_CONNECTED() 
@@ -122,11 +91,36 @@ BLYNK_CONNECTED()
   Blynk.syncAll();
 }
 
+int cnt_error = 0;
+
 void loop()
 {
-  Blynk.run();
-  timer.run();
+    Blynk.run();
+    timer.run();
 }
+
+//void loop()
+//{
+//Serial.println("-------->!!!");   
+//  if (Blynk.connected())
+//  {
+//    cnt_error= 0;
+//    Blynk.run();
+//    timer.run();
+//  }else
+//  {
+//    Serial.print("Blynk Server not connected!!!");    
+//    
+//    cnt_error++;
+//    if(cnt_error > 30)
+//    {
+//      cnt_error= 0;
+//      delay(1000);
+//      ESP.reset();
+//      delay(1000);      
+//    }
+//  }  
+//}
 
 /**
  * 타이머값 받기
